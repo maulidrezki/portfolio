f0cf948cd27c02be5aa81b8fda708c6cdd82a5c3
Maulid Rezki <maulidrezki13@gmail.com>
2022-01-27 16:54:47 +0700

    routes for projects fixed and now each project are scalable

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
-  get "project", to: "pages#project", as: "project"
-
+  get "projects/:id", to: "projects#show", as: "project"
 end
